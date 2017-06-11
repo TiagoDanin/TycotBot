2e6f902d51d339241256e97431f5dcaa07c866e3
dansenpir <dansenpir@protonmail.com>
2017-06-11 00:01:11 -0300

    Iniciando as boas-vindas

--- pybot.py
+++ pybot.py
@@ -1,27 +1,34 @@
 import telepot
 import time
 from datetime import datetime
+from telepot.loop import MessageLoop
+import json
 
 now = datetime.now()
-bot = telepot.Bot(str(input("TOKEN> ")))
+bot = telepot.Bot("386146227:AAHZX_y7hw4TQV3rAdaxtgJYKJ6ygGiaXfY")
 updates = bot.getUpdates()
 print(updates)
 
 print("Bot inicializado!")
 
-def contact_admin(msg):
-    bot.sendMessage(msg['chat']['id'], input("> "))
+def welcome(msg): #continuar daqui
+    if chat_type == 'supergroup':
+        if(new_member == True):
+            bot.sendMessage(msg['username'], 'seja bem-vindo!')
 
 
 def handle(msg): #Ainda faltam alguns dados a serem adicionados#
     #Dados do usuário/bot
     user_id = float(msg['from']['id']) #ID do usuário ou bot
     first_name = str(msg['from']['first_name']) #Primeiro nome
-    language_code = str(msg['from']['language_code']) #idioma do usuário
     #Dados do Chat
     chat_id = float(msg['chat']['id']) #ID do chat
     chat_type = str(msg['chat']['type']) #Tipo do bate-papo (privado, grupo...)
 
+    try:
+        chat_title = str(msg['chat']['title'])
+    except:
+        chat_title = ''
     try:
         text = str(msg['text'])
     except:
@@ -49,36 +56,31 @@ def handle(msg): #Ainda faltam alguns dados a serem adicionados#
     hour = str(now.hour)
     minute = str(now.minute)
     second = str(now.second)
+    time = (hour+':'+minute+':'+second+' -> '+day+'/'+month+'/'+year)
 
     users = open('users-register.txt', 'a')
     log = open('log_comands.txt', 'a')
 
 
-    if text == '/start':
+    if text == '/start' or '/start@PygrameirosBot':
         if chat_type == 'private':
-            print(username + " iniciou o bot!")
-            bot.sendMessage(chat_id, "Olá, eu sou o Pybot!\n")
-            bot.sendMessage(chat_id, "Você deseja entrar em contato com os admins do Pygrameiros?")
-            while True: #Parei aqui (ajustar depois)
-                if text == 'sim':
-                    bot.sendMessage(186027472, "Consulte o Pybot!")
-                    contact_admin(msg)
-                else:
-                    text = false
-        #Salvando os dados no arquivo users
+            print(username + " iniciou o bot em: " + time)
+            bot.sendMessage(chat_id, "Olá, eu sou o Pybot!\nFui criado pela galera do Pygrameiros para te ajudar a administrar teu grupo!")
+            #Salvando os dados no arquivo users
             users.write(str("log [" + day + "/" + month + "/" + year + "][" + hour + ":" + minute + ":" + second + "]"))
             users.write(str(" | Username: " + username + " | ID: " + str(user_id) + " | Comando usado: " + text + " | Chat: " + chat_type + "\n"))
             users.close()
             print("@"+username + " ("+ first_name + " " + last_name + ") " + " Iniciou o Bot - Dados salvos!")
-        elif chat_type == 'supergroup':
-            bot.sendMessage(chat_id, "Por favor, inicie uma conversa no privado!", reply_to_message_id=message_id)
-    elif text == '/id':
-        bot.sendMessage(chat_id, str("Pybot - Info ID \n\n *Nome:* " + username + " \n *ID:* " + user_id + " \n *Nome do Grupo:* " + chat_title + " \n *Id Group:* " + chat_id) , parse_mode="Markdown",reply_to_message_id=message_id)
+        """else:
+            bot.sendMessage(chat_id, "Por favor, entre em contato comigo pelo chat. Bots funcionam apenas desta forma.", reply_to_message_id=message_id)"""
+    elif text == '/info' or text == '/info@PygrameirosBot':
+        bot.sendMessage(str(chat_id), str("ID INFO \n\n NOME: " + username + " \n ID: " + str(user_id) + " \n NOME DO GRUPO: " + chat_title + " \n ID GROUP: " + str(chat_id)) ,reply_to_message_id=str(message_id))
         log.write(str("log [" + day + "/" + month + "/" + year + "][" + hour + ":" + minute + ":" + second + "]"))
-        log.write(str(" | Username: " + username + " | ID: " + user_id + " | Comando usado: " + text + " | ChatType: " + chat_type + " | Chat ID: " + chat_id + "\n"))
+        log.write(str(" | Username: " + username + " | ID: " + str(user_id) + " | Comando usado: " + text + " | ChatType: " + chat_type + " | Chat ID: " + str(chat_id) + "\n"))
         log.close()
         print("@"+username + " ("+ first_name + " " + last_name + ") " + " Usou o Bot! - Dados salvos!")
 
-bot.message_loop(handle)
+MessageLoop(bot, handle).run_as_thread()
+MessageLoop(bot, welcome).run_as_thread()
 while 1:
-    pass
+    time.sleep(10)
