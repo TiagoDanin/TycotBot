bbc9a8558eb93744e7603e50fecc246f41883a07
dansenpir <dansenpir@protonmail.com>
2017-08-15 21:07:58 -0300

    details

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ import sys
 from control_bot import control
 
 
-TOKEN = sys.argv[1]
+# TOKEN = "sys.argv[1]"
+TOKEN = "403341313:AAFTK0PYmwSk7wG5g-8xdVrzuoOPZzsOC9s"
 bot = telepot.Bot(TOKEN)
 
 def handle(msg):
     use_bot = control(msg, bot)
-    
+
     use_bot.log()
     use_bot.commands()
     use_bot.goodbye()
