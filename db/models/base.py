0d424b7e29cd0aa44eab5d021d6632027170f04f
LaBatata101 <labatata101@linuxmail.org>
2018-01-15 17:59:01 -0300

    add db and models folder add base.py in models

---
+++ db/models/base.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('postgresql://labatata101:#linuxfaustop1234@localhost:5432/sqlalchemy')
+
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
