146a452a878787c77c94871ee0e1eda65e19ca63
LaBatata101 <labatata101@linuxmail.org>
2018-01-17 18:41:11 -0300

    add method get_total_warns in queries.py

--- db/queries.py
+++ db/queries.py
@@ -1,4 +1,5 @@
 from models.base import Session
+from models.user import User
 
 session = Session()
 
@@ -10,5 +11,11 @@ def make_query(table, query):
     query -- should be like this example:
         User.name == 'kleber'
     '''
-    result = session.query(table).filter(query).all()
-    return result
+    return session.query(table).filter(query).all()
+
+
+def get_total_warns(username='', user_id=''):
+    if username:
+        return session.query(User.total_warns).filter_by(user_name=username).first()
+    else:
+        return session.query(User.total_warns).filter_by(user_id=user_id).first()
