1f7da22de939501bc995ad12e8729ed11851f290
LaBatata101 <labatata101@linuxmail.org>
2018-01-21 19:19:44 -0300

    add class attibute(metadata, bot)

--- generic_bot.py
+++ generic_bot.py
@@ -1,9 +1,18 @@
 import telepot
+from command.user import UserCmd
 
 
 '''
 extender classes de comando do admin e usario
 '''
-class GenericBot():
-    def __init__(self):
-        pass
+class GenericBot(UserCmd):
+    def __init__(self, bot, msg):
+        self.bot = bot
+        self.metadata = {'chat_type': msg['chat']['id'],
+                         'chat_id': msg['chat']['type'],
+                         'user_id': msg['from']['id'],
+                         'username': msg['from']['username'],
+                         'first_name': msg['from']['first_name'],
+                         'msg_id': msg['message_id']}
+        super().__init__(self.bot, self.metadata)
+
