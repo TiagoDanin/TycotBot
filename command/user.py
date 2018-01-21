e9740e7d5b3677b3b1aeedd2daa20a78d1c51319
LaBatata101 <labatata101@linuxmail.org>
2018-01-21 19:17:39 -0300

    invoke super constructor

--- command/user.py
+++ command/user.py
@@ -1,14 +1,16 @@
 
-class UserCmd:
+class UserCmd(object):
 
     def __init__(self, bot, metadata):
         self.bot = bot
         self.metadata = metadata
+        supe().__init__()
+
 
     def info(self):
         if self.metadata['chat_type'] == 'private':
             self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='Markdown',
                                  text=f'''*ID INFO*
-                                 _*NOME*_:{self.metadata['user']}
+                                 _*NOME*_:{self.metadata['username']}
                                  _*ID*_:{self.metadata['user_id']}''',
                                  reply_to_message_id=self.metadata['msg_id'])
