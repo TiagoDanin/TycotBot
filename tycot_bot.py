dfd62094dfd30035caba63312f137b413d4d227e
LaBatata101 <labatata101@linuxmail.org>
2018-01-22 15:31:03 -0300

    change class name to TycotBot

---
+++ tycot_bot.py
@@ -0,0 +1,15 @@
+from command.user import UserCmd
+
+
+class TycotBot(object):
+
+    def __init__(self, bot, msg):
+        self.bot = bot
+        self.metadata = {'chat_id': msg['chat']['id'],
+                         'chat_type': msg['chat']['type'],
+                         'user_id': msg['from']['id'],
+                         'username': msg['from']['username'],
+                         'first_name': msg['from']['first_name'],
+                         'msg_id': msg['message_id']}
+
+        self.usercmd = UserCmd(self.bot, self.metadata)
