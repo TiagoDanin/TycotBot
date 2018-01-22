0beafa419aa0a4e20916fa98275571c15289e6d7
LaBatata101 <labatata101@linuxmail.org>
2018-01-22 15:37:46 -0300

    add AdminCmd class

---
+++ command/admin.py
@@ -0,0 +1,10 @@
+
+class AdminCmd(object):
+    '''
+    This class represents the admin commands
+    '''
+
+    def __init__(self, bot, metadata):
+        self.bot = bot
+        self.metadata = metadata
+        super().__init__()
