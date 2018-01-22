2a0943483144ac0c64a5e7219457ae2fa9de89b9
LaBatata101 <labatata101@linuxmail.org>
2018-01-21 21:32:42 -0300

    add commands(info, help) and correct messages formatation

--- command/user.py
+++ command/user.py
@@ -7,9 +7,26 @@ class UserCmd(object):
         super().__init__()
 
     def info(self):
-        if self.metadata['chat_type'] == 'private':
-            self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='Markdown',
-                                 text=f'''*ID INFO*
-                                 _*NOME*_:{self.metadata['username']}
-                                 _*ID*_:{self.metadata['user_id']}''',
-                                 reply_to_message_id=self.metadata['msg_id'])
+        self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='html',
+                             text=('<b>ID INFO</b>\n'
+                                   '==================\n'
+                                   '<code>NOME</code> : {nome}\n'
+                                   '<code>ID</code>   : {id}').format(
+                                       nome=self.metadata['username'],
+                                       id=self.metadata['user_id']),
+                             reply_to_message_id=self.metadata['msg_id'])
+
+    def help(self):
+        help_msg = '''
+Olá, sou o Tycot!
+Segue minha lista de comandos:
+    /alert -> ativar serviço de alertas
+    /alertoff -> desativar serviço de alertas
+    /info -> informações do grupo
+    /link -> link do grupo
+    /regras -> regras do grupo
+    /leave -> sair do grupo
+    /verifybook -> verifica o ultimo livro do packtpub
+                          '''
+        self.bot.sendMessage(self.metadata['user_id'], (help_msg),
+                             reply_to_message_id=self.metadata['msg_id'])
