f1794d980be6806b6ff182485f8be8d0ed11d66e
LaBatata101 <labatata101@linuxmail.org>
2018-01-26 16:53:09 -0300

    change group_id type to Integer

--- db/models/group.py
+++ db/models/group.py
@@ -16,12 +16,12 @@ class Group(Base):
 
     id = Column(Integer, primary_key=True)
     group_name = Column(String)
-    group_id = Column(String)
+    group_id = Column(Integer)
     users = relationship("User", secondary=groups_user_association)
-    max_warns = Column(Integer)
+    max_warns = Column(String)
     welcome_msg = Column(Text)
 
-    def __init__(self, group_name='', group_id='', max_warns=0):
+    def __init__(self, group_name='', group_id=None, max_warns=0):
         self.group_name = group_name
         self.group_id = group_id
         self.max_warns = max_warns
