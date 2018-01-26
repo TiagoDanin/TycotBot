c7f5bed3c0cbef1dd71f08e0da58536e54ba3551
LaBatata101 <labatata101@linuxmail.org>
2018-01-26 16:51:12 -0300

    fix minor bugs and add start method

--- admin_cmd.py
+++ admin_cmd.py
@@ -1,4 +1,5 @@
-from db.inserts import set_welcome_msg
+from db.inserts import set_welcome_msg, addto_db, commit_and_close
+from db.models.group import Group
 
 
 class AdminCmd(object):
@@ -11,9 +12,15 @@ class AdminCmd(object):
         self.metadata = metadata
         super().__init__()
 
+    def start(self):
+        addto_db(Group(self.metadata['chat_name'], self.metadata['chat_id']))
+        commit_and_close()
+        self.bot.sendMessage(self.metadata['chat_id'], 'Seu grupo foi cadastrado com sucesso!',
+                             reply_to_message_id=self.metadata['msg_id'])
+
     def defwelcome(self, msg):
-        set_welcome_msg(self.metadata['group_id'],
+        set_welcome_msg(self.metadata['chat_id'],
                         msg.replace("/defwelcome ", ""))
-        self.bot.sendMessage(self.metadata['group_id'],
+        self.bot.sendMessage(self.metadata['chat_id'],
                              'As mensagens de boas-vindas foram alteradas com sucesso!',
-                             reply_to_message_id=self.msg_id)
+                             reply_to_message_id=self.metadata['msg_id'])
