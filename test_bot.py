7bedffc926fadd4b87485a0d2329b5c97a24556e
LaBatata101 <labatata101@linuxmail.org>
2018-01-26 16:53:55 -0300

    add welcome and goodbye msg

--- test_bot.py
+++ test_bot.py
@@ -9,15 +9,21 @@ bot = telepot.Bot(sys.argv[1])
 def handle(msg):
     tycot = TycotBot(bot, msg) 
     print(msg)
-    print(bot.getChatAdministrators(msg['chat']['id']))
+    if 'new_chat_member' in msg:
+        tycot.new_member(msg)
+    elif 'left_chat_member' in msg:
+        tycot.goodbye(msg)
+
     if msg['text'] == '/info':
         tycot.usercmd.info()
     elif msg['text'] == '/link':
         print(bot.exportChatInviteLink(msg['chat']['id']))
     elif msg['text'] == '/ajuda':
         tycot.usercmd.help()
-    elif msg['text'] == '/defwelcome':
+    elif msg['text'].startswith('/defwelcome'):
         tycot.admcmd.defwelcome(msg['text'])
+    elif msg['text'] == '/start':
+        tycot.admcmd.start()
 
 
 if __name__ == '__main__':
