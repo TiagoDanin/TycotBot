6f9782a98a46a7705497af8ea40b79c8b2c548df
LaBatata101 <labatata101@linuxmail.org>
2018-02-02 19:36:42 -0300

    add methods (filter_by_group_id, get_link) and update other methods

--- db/models/base.py
+++ db/models/base.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('postgresql://usr:pass@localhost:5432/tycot')
+engine = create_engine('postgresql://labatata101:#linuxfaustop1234@localhost:5432/tycot', echo=True)
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
--- db/queries.py
+++ db/queries.py
@@ -15,22 +15,26 @@ def make_query(table, query):
     return session.query(table).filter(query).all()
 
 
+def filter_by_group_id(table, value):
+    return session.query(table).filter_by(group_id=value).first()
+
+
 def get_total_warns(username='', user_id=''):
     '''
     Get total warns from a spefic user. You can choose which parameter to use, for filter
     the user.
-    username -- Name of the user
     user_id -- id of the user
     '''
-    if username:
-        return session.query(User.total_warns).filter_by(user_name=username).first()
-    else:
-        return session.query(User.total_warns).filter_by(user_id=user_id).first()
+    return session.query(User.total_warns).filter_by(user_id=user_id).first()[0]
 
 
 def get_welcome_msg(group_id):
-    return session.query(Group.welcome_msg).filter_by(group_id=group_id).first()[0]
+    return filter_by_group_id(Group.welcome_msg, group_id)[0]
 
 
 def get_rules(group_id):
-    return session.query(Group.rules).filter_by(group_id=group_id).first()[0]
+    return filter_by_group_id(Group.rules, group_id)[0]
+
+
+def get_link(group_id):
+    return filter_by_group_id(Group.link, group_id)[0]
