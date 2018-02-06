17f92683cedb641832235b2ec6e0367fc2272215
LaBatata101 <labatata101@linuxmail.org>
2018-02-06 13:00:00 -0300

    update requirements

--- db/models/base.py
+++ db/models/base.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('postgresql://labatata101:#linuxfaustop1234@localhost:5432/tycot', echo=True)
+engine = create_engine('postgresql://usr:pass@localhost:5432/tycot')
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
