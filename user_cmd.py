6b3bd1d84240331ffce3f3781a9a7effe81eea09
LaBatata101 <labatata101@linuxmail.org>
2018-02-06 13:01:37 -0300

    update methods (info, link, help)

--- user_cmd.py
+++ user_cmd.py
@@ -1,4 +1,4 @@
-from db.queries import get_rules, get_link
+from db.queries import get_rules, get_link, get_max_warns
 
 
 class UserCmd(object):
@@ -15,27 +15,40 @@ class UserCmd(object):
         '''
         Show information about the user or group
         '''
-        self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='html',
-                             text=('<b>ID INFO</b>\n'
-                                   '==================\n'
-                                   '<code>NOME</code> : {nome}\n'
-                                   '<code>ID</code>   : {id}').format(
-                                       nome=self.metadata['username'],
-                                       id=self.metadata['user_id']),
-                             reply_to_message_id=self.metadata['msg_id'])
+        if self.metadata['chat_type'] != 'private':
+            msg = ('<b>ID INFO</b>\n'
+                   '==================\n'
+                   '<code>ID DO GRUPO</code> : {chat_id}\n'
+                   '<code>TOTAL DE ADVERTENCIAS</code> : {max_warn}\n'
+                   '<code>NOME</code> : {nome}\n'
+                   '<code>ID</code>   : {id}')
+            self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='html',
+                                 text=msg.format(chat_id=self.metadata['chat_id'],
+                                                 max_warn=get_max_warns(self.metadata['chat_id']),
+                                                 nome=self.metadata['username'],
+                                                 id=self.metadata['user_id']),
+                                 reply_to_message_id=self.metadata['msg_id'])
+        else:
+            msg = ('<b>ID INFO</b>\n'
+                   '==================\n'
+                   '<code>NOME</code> : {nome}\n'
+                   '<code>ID</code>   : {id}') 
+            self.bot.sendMessage(chat_id=self.metadata['chat_id'], parse_mode='html',
+                                 text=msg.format(nome=self.metadata['username'],
+                                                 id=self.metadata['user_id']),
+                                 reply_to_message_id=self.metadata['msg_id'])
 
     def help(self):
         help_msg = '''
 Olá, sou o Tycot!
 Segue minha lista de comandos:
+    /ajuda -> mostra essa mensagem
     /info -> informações do grupo
     /link -> link do grupo
     /regras -> regras do grupo
-    /leave -> sair do grupo
     /verifybook -> verifica o ultimo livro do packtpub
                           '''
-        self.bot.sendMessage(self.metadata['user_id'], help_msg,
-                             reply_to_message_id=self.metadata['msg_id'])
+        self.bot.sendMessage(self.metadata['user_id'], help_msg)
 
     def rules(self):
         rules = get_rules(self.metadata['chat_id'])
@@ -46,11 +59,11 @@ Segue minha lista de comandos:
             self.bot.sendMessage(self.metadata['chat_id'], '*Sem regras!*', parse_mode='Markdown')
 
     def link(self):
-        # if self.metadata['chat_type'] == 'supergroup':
-            # link = self.bot.exportChatInviteLink(self.metadata['chat_id'])
-            # self.bot.sendMessage(self.metadata['chat_id'], link,
-                                 # reply_to_message_id=self.metadata['msg_id'])
-        # else:
-        link = get_link(self.metadata['chat_id'])
-        self.bot.sendMessage(self.metadata['chat_id'], link,
-                             reply_to_message_id=self.metadata['msg_id'])
+        if self.metadata['chat_type'] == 'supergroup':
+            link = self.bot.exportChatInviteLink(self.metadata['chat_id'])
+            self.bot.sendMessage(self.metadata['chat_id'], link,
+                                 reply_to_message_id=self.metadata['msg_id'])
+        else:
+            link = get_link(self.metadata['chat_id'])
+            self.bot.sendMessage(self.metadata['chat_id'], link,
+                                 reply_to_message_id=self.metadata['msg_id'])
