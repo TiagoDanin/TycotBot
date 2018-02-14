0f2f581e941bc6eeb32edef813dfd5677fc09db5
LaBatata101 <labatata101@linuxmail.org>
2018-02-14 15:03:33 -0300

    add general docs

--- db/queries.py
+++ db/queries.py
@@ -14,35 +14,52 @@ def make_query(table, query):
 
 
 def get_user(user_id):
+    '''
+    Query a user from the db specified by his id.
+    Return a User object. 
+    user_id -- id of user
+    '''
     return make_query(User, User.user_id == user_id)[0]
 
 
-def filter_by_group_id(table, value):
-    return session.query(table).filter_by(group_id=value).first()
+def filter_by_group_id(column, group_id):
+    '''
+    Make a query to the db and return the first result.
+    column -- column to query
+    group_id -- id of the group
+    '''
+    return session.query(column).filter_by(group_id=group_id).first()
 
 
 def get_max_warns(group_id):
     '''
-    Get total warns from a spefic user. You can choose which parameter to use, for filter
-    the user.
-    group_id -- id of the user
+    Get the max warn from the group 
+    group_id -- id of the group
     '''
     return filter_by_group_id(Group.max_warns, group_id)[0]
 
 
 def get_welcome_msg(group_id):
+    '''Return the welcome message of the group.
+    group_id -- id of the group'''
     return filter_by_group_id(Group.welcome_msg, group_id)[0]
 
 
 def get_rules(group_id):
+    '''Return the rules of the group.
+    group_id -- id of the group'''
     return filter_by_group_id(Group.rules, group_id)[0]
 
 
 def get_link(group_id):
+    '''Return the link of the group.
+    group_id -- id of the group'''
     return filter_by_group_id(Group.link, group_id)[0]
 
 
-def chat_exist(group_id):
+def group_exist(group_id):
+    '''Verify if the group is in the database.
+    group_id -- id of the group'''
     if make_query(Group, Group.group_id == group_id):
         return True
     return False
